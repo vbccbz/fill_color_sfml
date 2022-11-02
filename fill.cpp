6f0216dfc58b12a62ce478a9ea3badb56596b764
George <31924134+vbccbz@users.noreply.github.com>
2022-11-02 10:15:58 +0600

    Add files via upload

--- fill.cpp
+++ fill.cpp
@@ -16,31 +16,88 @@ public:
     //std::cout << '|' << red << ' ' << green << ' ' << blue << '|';
     std::cout << ' ' << red + green + blue << ' ';
   }
+  bool operator== (Pixel const& p)
+  {
+    bool result = false;
+    if (p.red == red && p.green == green && p.blue == blue)
+    {
+      result = true;
+    }
+    return result;
+  }
 };
 
 class Canvas
 {
 private:
-  std::vector < std::vector <Pixel> > buffer;
+  std::vector < std::vector <Pixel> > pixels;
 public:
   Canvas(void)
   {
-    buffer.resize(W, std::vector <Pixel>(H));
+    pixels.resize(W, std::vector <Pixel>(H));
   }
   void display(void) {
-    for (size_t red = 0; red < buffer.size(); red++)
+    for (size_t r = 0; r < pixels.size(); r++)
     {
-      for (size_t c = 0; c < buffer[0].size(); c++)
+      for (size_t c = 0; c < pixels[0].size(); c++)
       {
-        buffer[red][c].print();
+        pixels[r][c].print();
       }
       std::cout << '\n';
     }
+    std::cout << "____________" << '\n';
   }
-  void setColor(int x, int y, Pixel const & c)
+  void setColor(int y, int x, Pixel const& c)
   {
-    buffer[y][x] = c;
+    pixels[y][x] = c;
+  }
+  void fill(int y, int x, int r, int g, int b)
+  {
+    Pixel need(r, g, b);
+    Pixel base(pixels[y][x]);
 
+    struct Coordinates
+    {
+      int y;
+      int x;
+      Coordinates(int yc, int xc) : y(yc), x(xc) {}
+    };
+
+    Coordinates currentPosition(y, x);
+
+    std::stack <Coordinates> stackCoordinates;
+    stackCoordinates.push(currentPosition);
+    while (!stackCoordinates.empty())
+    {
+      currentPosition = stackCoordinates.top();
+      stackCoordinates.pop();
+
+      pixels[currentPosition.y][currentPosition.x] = need;
+      display();
+
+      Coordinates up(currentPosition.y - 1, currentPosition.x);
+      Coordinates down(currentPosition.y + 1, currentPosition.x);
+      Coordinates left(currentPosition.y, currentPosition.x - 1);
+      Coordinates right(currentPosition.y, currentPosition.x + 1);
+
+      if (up.y >= 0)
+      {
+        if (pixels[up.y][up.x] == base) { stackCoordinates.push(up); }
+      }
+      if (down.y < H)
+      {
+        if (pixels[down.y][down.x] == base) { stackCoordinates.push(down); }
+      }
+      if (left.x >= 0)
+      {
+        if (pixels[left.y][left.x] == base) { stackCoordinates.push(left); }
+      }
+      if (right.x < W)
+      {
+        if (pixels[right.y][right.x] == base) { stackCoordinates.push(right); }
+      }
+
+    }
   }
 };
 
@@ -49,16 +106,17 @@ int main(void)
   Canvas canvas;
   canvas.display();
 
-  int x; int y;
+  int y; int x;
   int red; int green; int blue;
   while (1)
   {
-    std::cin >> x;
     std::cin >> y;
+    std::cin >> x;
     std::cin >> red;
     std::cin >> green;
     std::cin >> blue;
-    canvas.setColor(x, y, Pixel(red,green,blue) );
+    //canvas.setColor(y, x, Pixel(red, green, blue));
+    canvas.fill(y, x, red, green, blue);
     canvas.display();
 
   }
